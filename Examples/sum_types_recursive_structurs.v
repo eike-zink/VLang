d4e845347bff024d9c7b54aa027ce073ab3565e0
eike-zink <user@eike-zink.de>
2024-03-21 21:26:09 +0100

    Create sum_types_recursive_structurs.v
    Beispiel aus der Dokumentation zum Sum-Type

---
+++ Examples/sum_types_recursive_structurs.v
@@ -0,0 +1,28 @@
+// Beispiel aus der Dokumentation zu V
+// V`s binary tree
+
+struct Empty {}
+
+struct Node {
+	value int
+	left  Tree
+	right Tree
+}
+
+type Tree = Empty | Node
+
+// sum up all node values
+
+fn sum(tree Tree) int {
+	return match tree {
+		Empty { 0 }
+		Node { tree.value + sum(tree.left) + sum(tree.right) }
+	}
+}
+
+fn main() {
+	left := Node{2, Empty{}, Empty{}}
+	right := Node{3, Empty{}, Node{4, Empty{}, Empty{}}}
+	tree := Node{5, left, right}
+	println(sum(tree)) // 2 + 3 + 4 + 5 = 14
+}
