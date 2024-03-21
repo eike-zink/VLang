1cac2914ef64e979a444642ee223c3768fbb30ac
eike-zink <user@eike-zink.de>
2024-03-21 19:48:42 +0100

    Update operator_overloading.v
    Die Funktion um einen Sum-Type erweitert.

--- Examples/operator_overloading.v
+++ Examples/operator_overloading.v
@@ -8,39 +8,55 @@ fn (a Vec) str() string {
 }
 
 fn (a Vec) + (b Vec) Vec {
-	return Vec{
-		a.x + b.x,
-		a.y + b.y
-	}
+	return Vec{a.x + b.x, a.y + b.y}
 }
 
 fn (a Vec) - (b Vec) Vec {
-	return Vec{
-		a.x - b.x,
-		a.y - b.y
-	}
+	return Vec{a.x - b.x, a.y - b.y}
 }
 
 fn (a Vec) * (b Vec) Vec {
-	return Vec{
-		a.x * b.x,
-		a.y * b.y
-	}
+	return Vec{a.x * b.x, a.y * b.y}
 }
 
 fn (a Vec) / (b Vec) Vec {
-	return Vec{
-		a.x / b.x,
-		a.y / b.y
+	return Vec{a.x / b.x, a.y / b.y}
+}
+
+type Faktor = Vec | int
+
+// ACHTUNG eine Operator-Overloading ist hier leider
+// nicht möglich, da dies nur bei Structs und Type alias
+// zulässig ist (???)
+
+fn (a Vec) multiply(f Faktor) Vec {
+	return match f {
+		Vec { a * f }
+		int { Vec{a.x * f, a.y * f} }
+	}
+}
+
+fn (a Vec) divide(f Faktor) Vec {
+	return match f {
+		Vec { a / f }
+		int { Vec{a.x / f, a.y / f} }
 	}
 }
 
 fn main() {
- 	a := Vec{x: 4, y: 4}
- 	b := Vec{x: 2, y: 2}
+	a := Vec{
+		x: 4
+		y: 4
+	}
+	b := Vec{
+		x: 2
+		y: 2
+	}
 
- 	println(a + b) // ==> {6, 6}
- 	println(a - b) // ==> {2, 2}
- 	println(a * b) // ==> {8, 8}
- 	println(a / b) // ==> {2, 2}
+	println(a + b)         // ==> {6, 6}
+	println(a - b)         // ==> {2, 2}
+	println(a * b)         // ==> {8, 8}
+	println(a / b)         // ==> {2, 2}
+	println(a.multiply(3)) // ==> {12, 12}
+	println(a.divide(2))   // ==> {2, 2}
 }
