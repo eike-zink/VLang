136d61efb84a0355c844f3f33ce6b1e932b48d6f
eike-zink <user@eike-zink.de>
2024-03-25 20:41:22 +0100

    Update walker.v
    Einführung von rows und cols, die Positionen des Walkers werden jetzt in einen Grid gespeichert.
    ACHTUNG, diese Lösung führt dazu, dass das Programm abbricht, wenn zuviele Positionen dargestellt werden sollen (siehe Dokumentation von gg)

--- Exercise/RandomWalker/walker.v
+++ Exercise/RandomWalker/walker.v
@@ -1,15 +1,31 @@
 import rand
+import math
 import gg
 import gx
 
-const directions :=[
-  Vector{x: -2, y: 0},  // left
-  Vector{x: 2, y: 0},   // right
-  Vector{x: 0, y: 2},	// up
-  Vector{x: 0, y: -2}   // down
+const directions = [
+	Vector{
+		x: -1
+		y: 0
+	}, // left
+	Vector{
+		x: 1
+		y: 0
+	}, // right
+	Vector{
+		x: 0
+		y: 1
+	}, // up
+	Vector{
+		x: 0
+		y: -1
+	}, // down
 ]
 
-const speed := Vector{x: 2, y: 2}
+const width = 400
+const height = 400
+
+const spacing = 5
 
 struct Walker {
 	Vector
@@ -20,29 +36,39 @@ fn (walker Walker) str() string {
 }
 
 fn (mut walker Walker) move() {
-	new_direction := rand.element(directions) or { panic('no direction')}
-	walker.Vector = walker.Vector + (new_direction * speed)
+	new_direction := rand.element(directions) or { panic('no direction') }
+	walker.Vector = walker.Vector + new_direction
 }
 
+fn (walker Walker) save_position(mut pos []Vector) {
+	pos << walker.Vector
+}
 
 struct App {
 mut:
-	gg     &gg.Context = unsafe { nil }
-	walker Walker
+	gg        &gg.Context = unsafe { nil }
+	walker    Walker
+	cols      int
+	rows      int
+	positions []Vector
 }
 
 fn main() {
 	mut app := &App{}
 	app.gg = gg.new_context(
-		width: 600
-		height: 600
+		width: width
+		height: height
 		window_title: 'Random Walker'
 		frame_fn: frame
 		user_data: app
 	)
+
+	app.cols = int(math.floor(width / spacing))
+	app.rows = int(math.floor(height / spacing))
+
 	app.walker = Walker{
-		x: 300
-		y: 300
+		x: app.cols / 2
+		y: app.rows / 2
 	}
 	app.gg.run()
 }
@@ -50,12 +76,14 @@ fn main() {
 fn frame(mut app App) {
 	app.gg.begin()
 	app.walker.move()
+	app.walker.save_position(mut app.positions)
 	app.draw()
-	// Option verhindert, dass der Context neu gezeichnet
-	// wird, aber jetzt wird der Background rot dargestellt
-	app.gg.end(how: .passthru)
+	app.gg.end()
 }
 
 fn (app &App) draw() {
-	app.gg.draw_circle_filled(app.walker.x, app.walker.y, 4, gx.rgba(255, 255, 120, 60))
+	for position in app.positions {
+		app.gg.draw_circle_filled(position.x * spacing, position.y * spacing, 4, gx.rgba(255,
+			255, 120, 120))
+	}
 }
